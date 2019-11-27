2767bcb6fef6dd24e9e0ed7ac1379c90a6cf408f
ysfelouardi <youssef.94.elouardi@gmail.com>
2019-11-27 10:36:51 +0100

    add prj files

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Todos from "./Todos.svelte";
+</script>
+
+<main>
+  <Todos />
+</main>
---
+++ src/Todos.svelte
@@ -0,0 +1,40 @@
+<script>
+  let todos = [];
+  let todo = "";
+
+  function handleAddTodo() {
+    todos = [todo, ...todos];
+    todo = "";
+  }
+
+  function handleCheckToDo(todoIndex) {
+    todos = todos.filter((todo, index) => todoIndex !== index);
+  }
+  //$: console.log({ todos });
+</script>
+
+<style>
+  li {
+    padding: 10px;
+    margin: 2px;
+  }
+  ul {
+    list-style: square;
+  }
+  span {
+    cursor: pointer;
+    color: cadetblue;
+  }
+</style>
+
+<h1>Simple Todos App</h1>
+<input type="text" bind:value={todo} />
+<button on:click={handleAddTodo}>Add</button>
+<ul>
+  {#each todos as todoItem, index}
+    <li>
+      {todoItem}
+      <span on:click={() => handleCheckToDo(index)}>check</span>
+    </li>
+  {/each}
+</ul>
